f25cb0886433749f6e6de90881ba0a4790c6b232
Jollen Chen <jollen@moko365.com>
2016-03-27 22:36:24 +0800

    Publish react example: slideshow

--- dist/hello.js
+++ dist/hello.js
@@ -1,6 +1,34 @@
-'use strict';
+"use strict";
 
-var NameInput = React.createClass({ displayName: "NameInput",
+var SlideShowButton = React.createClass({ displayName: "SlideShowButton",
+  handleClick: function handleClick(event) {
+    event.preventDefault();
+
+    // use Flux pattern to arrange this
+    $(this.props.hideTarget).addClass('hide');
+
+    var slideshow = remark.create();
+  },
+  render: function render() {
+    return React.createElement("input", {
+      type: "submit",
+      onClick: this.handleClick,
+      className: "btn btn-default",
+      value: this.props.value });
+  }
+});
+
+var MarkdownTextArea = React.createClass({ displayName: "MarkdownTextArea",
+  componentDidMount: function componentDidMount() {},
+  render: function render() {
+    return React.createElement("textarea", {
+      rows: this.props.rows,
+      className: this.props.className,
+      onChange: this.props.handleChange });
+  }
+});
+
+var MarkdownLiveEditor = React.createClass({ displayName: "MarkdownLiveEditor",
   markdownGithubParser: function markdownGithubParser(data) {
     var self = this;
     $.ajax({
@@ -11,27 +39,60 @@ var NameInput = React.createClass({ displayName: "NameInput",
       data: data,
       contentType: "text/plain",
       success: function success(data) {
-        $(self.props.target).html(data);
+        self.setState({
+          data: data
+        });
       },
       error: function error(xhr, status, err) {
-        console.log(err + ': ' + status);
+        self.setState({
+          data: status
+        });
       }
     });
   },
   markdownLocalParser: function markdownLocalParser(data) {
-    var converter = new showdown.Converter();
-    $(this.props.target).html(converter.makeHtml(data));
+    var data = this.converter.makeHtml(data);
+    this.setState({
+      data: data
+    });
+  },
+  parser: function parser(data) {
+    if (typeof this.markdownParsers[this.props.markdownParser] === "function") {
+      this.markdownParsers[this.props.markdownParser](data);
+    }
+  },
+  componentDidMount: function componentDidMount() {
+    this.converter = new showdown.Converter();
+
+    this.markdownParsers = {
+      local: this.markdownLocalParser,
+      github: this.markdownGithubParser
+    };
+  },
+  getInitialState: function getInitialState() {
+    return {
+      data: ''
+    };
   },
   handleChange: function handleChange(event) {
-    if (this.props.parser && this.props.parser === "local") this.markdownLocalParser(event.target.value);else this.markdownGithubParser(event.target.value);
+    // Parse markdown
+    this.parser(event.target.value);
+
+    // use Flux pattern to arrange this
+    $('#source').text(event.target.value);
   },
   render: function render() {
-    return React.createElement("textarea", {
+    // Live preview
+    $(this.props.livePreviewTarget).html(this.state.data);
+
+    return React.createElement(MarkdownTextArea, {
+      handleChange: this.handleChange,
       rows: this.props.rows,
-      className: "form-control",
-      onChange: this.handleChange });
+      livePreviewTarget: this.props.target,
+      className: this.props.className,
+      markdownParser: this.props.markdownParser });
   }
 });
 
-ReactDOM.render(React.createElement(NameInput, { rows: "5", target: "#article", parser: "local" }), document.getElementById('container'));
+ReactDOM.render(React.createElement("div", { className: "container" }, React.createElement("div", { className: "row" }, React.createElement("div", { className: "col-md-6 col-xs-6 col-lg-6", id: "container" }, React.createElement(SlideShowButton, { hideTarget: "#editor", value: "Slide Now" }), React.createElement(MarkdownLiveEditor, { className: "form-control", rows: "25", livePreviewTarget: "#article", markdownParser: "local" })), React.createElement("div", { className: "col-md-6 col-xs-6 col-lg-6", id: "article" }))), document.getElementById('editor'));
 //# sourceMappingURL=hello.js.map
--- src/hello.jsx
+++ src/hello.jsx
@@ -1,4 +1,37 @@
-var NameInput = React.createClass({
+var SlideShowButton = React.createClass({
+  handleClick: function(event) {
+    event.preventDefault();
+
+    // use Flux pattern to arrange this
+    $(this.props.hideTarget).addClass('hide');
+
+    var slideshow = remark.create();
+  },
+  render: function() {
+    return (
+        <input 
+        type="submit" 
+        onClick={this.handleClick}
+        className="btn btn-default"
+        value={this.props.value} />
+    )
+  }
+});
+
+var MarkdownTextArea = React.createClass({
+  componentDidMount: function() {
+  },
+  render: function() {
+    return (
+      <textarea 
+      rows={this.props.rows}
+      className={this.props.className}
+      onChange={this.props.handleChange} />
+    );
+  }
+});
+
+var MarkdownLiveEditor = React.createClass({
   markdownGithubParser: function(data) {
     var self = this;
     $.ajax({
@@ -9,34 +42,73 @@ var NameInput = React.createClass({
       data: data,
       contentType: "text/plain",
       success: function(data) {
-        $(self.props.target).html(data);
+        self.setState({
+          data: data
+        });
       },
       error: function(xhr, status, err) {
-        console.log(err + ': ' + status);
+        self.setState({
+          data: status
+        });
       }
     });
   },
   markdownLocalParser: function(data) {
-    var converter = new showdown.Converter();
-    $(this.props.target).html(converter.makeHtml(data));
+    var data = this.converter.makeHtml(data);
+    this.setState({
+      data: data
+    });
+  },
+  parser: function(data) {
+    if (typeof this.markdownParsers[this.props.markdownParser] === "function") {
+      this.markdownParsers[this.props.markdownParser](data);
+    }
+  },
+  componentDidMount: function() {
+    this.converter = new showdown.Converter();
+
+    this.markdownParsers = {
+      local: this.markdownLocalParser,
+      github: this.markdownGithubParser
+    };
+  },
+  getInitialState: function() {
+    return {
+      data: ''
+    };
   },
   handleChange: function(event) {
-    if (this.props.parser && this.props.parser === "local")
-      this.markdownLocalParser(event.target.value);
-    else
-      this.markdownGithubParser(event.target.value);
+    // Parse markdown
+    this.parser(event.target.value);
+
+    // use Flux pattern to arrange this
+    $('#source').text(event.target.value)
   },
   render: function() {
+    // Live preview
+    $(this.props.livePreviewTarget).html(this.state.data);
+
     return (
-      <textarea 
-      rows={this.props.rows}
-      className="form-control" 
-      onChange={this.handleChange} />
-    );
+        <MarkdownTextArea 
+        handleChange={this.handleChange} 
+        rows={this.props.rows}
+        livePreviewTarget={this.props.target} 
+        className={this.props.className}
+        markdownParser={this.props.markdownParser} />
+    )
   }
 });
 
 ReactDOM.render(
-  <NameInput rows="5" target="#article" parser="local" />,
-  document.getElementById('container')
-);
+  <div className="container">
+    <div className="row">
+      <div className="col-md-6 col-xs-6 col-lg-6" id="container">
+        <SlideShowButton hideTarget="#editor" value="Slide Now" />
+        <MarkdownLiveEditor className="form-control" rows="25" livePreviewTarget="#article" markdownParser="local" />
+      </div>
+      <div className="col-md-6 col-xs-6 col-lg-6" id="article">
+      </div>
+    </div>
+  </div>
+  ,
+  document.getElementById('editor'));
